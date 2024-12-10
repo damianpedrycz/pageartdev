873ca6e554d1819d7c82320e335875193c69af05
Damian Pedrycz <damian.pedrycz@pageart.pl>
2024-12-10 14:41:49 +0100

    Change container width

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,9 +14,9 @@ const config = {
     container: {
       center: true,
       padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+      // screens: {
+      //   "2xl": "1400px",
+      // },
     },
     extend: {
       fontFamily: {
